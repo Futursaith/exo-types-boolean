a224d0cb58a5c8ff973b9f1d6094e8396e1fa8f3
Futursaith <saith_7@hotmail.fr>
2021-02-15 19:37:56 +0100

    exo 3

---
+++ coat.js
@@ -0,0 +1,16 @@
+let isRainy = true
+let isWindy = false
+let goodtemperature = 20
+
+if (isRainy || isWindy) {
+  console.log('Mettez votre manteau')
+}
+else if (temperature <= 10 && israiny) {
+  console.log('besoin du gros manteau')
+}
+else if (temperature >= 25 && iswindy) {
+  console.log('une simple veste suffira')
+}
+else {
+  console.log('le manteau oui mais pas de tous temps')
+}
