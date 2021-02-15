d6e3ed79c2751cdb1efae284b0f2fa7f936fab2f
Futursaith <saith_7@hotmail.fr>
2021-02-15 19:06:30 +0100

    exo 2

---
+++ trading.js
@@ -0,0 +1,10 @@
+let bitcoin = 100;
+
+if (bitcoin >= 0 && bitcoin <= 1000) {
+  console.log('go acheter du bit')
+}
+else if (bitcoin >= 1000 && bitcoin < 1000) {
+  console.log('attend un peut ca sens pas bon')
+}
+else {
+  console.log('vend tout ce tu as')
